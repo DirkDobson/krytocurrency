ce7293d04601c91f004ea4f6f4aaa8731f53d969
Dirk Dobson <dirkvancouver@live.com>
2018-10-08 10:56:38 -0600

    reducers

---
+++ client/src/reducers/coin.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const COINS = 'COINS'
+const ADD_COIN = 'ADD_COIN'
+const REMOVE_COIN = 'REMOVE_COIN'
+
+export const get = () => {
+  return (dispatch) => {
+    axios.get('/api/coins')
+      .then( res => dispatch({type: COINS, coins: res.data}) )
+  }
+}
+
+export const addCoin = (coin) => {
+  return ( dispatch ) => {
+    axios.post('/api/coins', { coin })
+      .then( res => dispatch({ type: ADD_COIN, coin: res.data }) )
+  }
+}
+
+export const removeCoin = (id) => {
+  return ( dispatch ) => {
+   axios.put(`/api/coins/${id}`)
+  .then( () => dispatch({ type: REMOVE_COIN, id }) )
+  }
+}
+
+export default ( state = [], action ) => {
+  switch(action.type) {
+    case COINS:
+    case REMOVE_COIN:
+    default:
+      return state
+  }
+}
